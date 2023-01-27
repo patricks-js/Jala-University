0de680fe906b0bb8c6b4e5bbff242710765b3f18
Lucas Patrick <lucas.patrick.lsilva@gmail.com>
2023-01-27 10:56:09 -0300

    finished 3.0

--- Programming/week_1/pseudo-code-js/5-prime-number.js
+++ Programming/week_1/pseudo-code-js/5-prime-number.js
@@ -3,16 +3,20 @@
 function isPrime(value) {
   let dividers = 0;
 
+  if (value === 0) {
+    return "Null number";
+  }
+
   for (let i = 1; i <= value; i++) {
     if (value % i === 0) {
       dividers += 1;
     }
   }
 
-  if (dividers > 2 || value == 1) {
+  if (dividers > 2 || value <= 1) {
     return `${value} not is prime`;
   }
   return `${value} is prime`;
 }
 
-console.log(isPrime(9));
+console.log(isPrime(10));
